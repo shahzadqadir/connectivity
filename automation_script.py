6b55f88cffb91986e67fd6e2034177a53297c111
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2025-06-10 06:05:17 +0100

    completed inter-switch trunking configurations

--- automation_script.py
+++ automation_script.py
@@ -30,6 +30,27 @@ def show_vlans(switches_list: list, username: str, password: str):
         for line in output:
             print(line)
 
+def configure_trunk(switch: str, username: str, password: str, 
+                    port: str, vlans: list):
+    """ Configure port as trunk.
+    Allows vlans provided in the list of strings"""
+    allowed_vlans = ','.join(vlans)    
+    connection = Connectivity(switch)
+    connection.login(username=username, password=password)
+    connection.config_cmd_ssh(
+        ["configure terminal", 
+         f"interface {port}",
+         "switchport trunk encapsulation dot1q",
+         "switchport mode trunk",
+         f"switchport trunk allowed vlan {allowed_vlans}",
+         "end"]
+        )
+    # config_cmd_ssh closes connection so we need to open it again
+    connection = Connectivity(switch)
+    connection.login(username=username, password=password)
+    output = connection.execute_show_commands("show int trunk")
+    return output
+
 if __name__ == "__main__":
     switches = ["10.10.99.13", "10.10.99.14"]
     create_vlans(switches_list=switches,
@@ -37,6 +58,22 @@ if __name__ == "__main__":
                  username='script', password='cisco123')
     show_vlans(switches_list=switches, 
                username='script', password='cisco123')
+    switch = "10.10.99.13"
+    output = configure_trunk(switch=switch, username="script", password="cisco123", 
+                    port="Ethernet0/0", vlans=['11', '12', '13'])
+    print(f"{switch} RESULTS")
+    for line in output:
+        print(line)
+    
+    switch = "10.10.99.14"
+    output = configure_trunk(switch=switch, username="script", password="cisco123", 
+                    port="Ethernet0/0", vlans=['11', '12', '13'])
+    print("#################")
+    print(f"{switch} Results")
+    for line in output:
+        print(line)
+
+    
 
 
 
